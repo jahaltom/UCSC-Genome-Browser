21e68a9b2c0ddc102dad184cfa0fc80571015204
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2023-12-20 15:58:17 -0500

    Update UCSCGbrowsTrackGenerator.py

--- UCSCGbrowsTrackGenerator.py
+++ UCSCGbrowsTrackGenerator.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import csv
-
+import numpy as np
 
 
 
 ##Makes bed detailed tracks. By chr. 
 
 md = pd.read_csv("Transcript_level_metadata.tsv",sep='\t')
+md=md.drop(columns=['isHighlyExpressed',"COVID-19 expressed"])   
+
 
 covid = pd.read_csv("CovidEBs.txt",sep='\t',header=None)
 covid.columns =['TranscriptID']
@@ -15,16 +17,16 @@ non.columns =['TranscriptID']
 
 
 #Get useful attributes for gtf
-md['attributes']= "Gene ID Version: " + md['Gene_ID_ver'] + "    Transcript ID: " + md['TranscriptID'] + "    ORF length: " + md['ORF_length'].astype(str) + "    cDNA length: " + md['cdna_length'].astype(str) +"    Final Strata: " + md['final_strata'].fillna('').astype('string')   
+md['attributes']= "Gene ID Version: " + md['Gene_ID_ver'] + "    Transcript ID: " + md['TranscriptID'] + "    ORF length: " + md['ORF_length'].astype(str) + "    cDNA length: " + md['cdna_length'].astype(str) +"    Phylostrata: " + md['final_strata'].fillna('').astype('string') +"    Ribo-Seq evidence: " + md['RiboSeq-Status'].fillna('').astype('string') +"    Covid-19 expressed: "
 
 
 
-non["source"]="EB"
-covid["source"]="COVID-19 expressed EB"
+non["COVID-19 expressed"]=""
+covid["COVID-19 expressed"]="+"
 
 _54k=md[md["is54K_EB"]==True]
-_54k["source"]="EB"
-_54k=_54k[["TranscriptID","source"]]
+_54k["COVID-19 expressed"]=""
+_54k=_54k[["TranscriptID","COVID-19 expressed"]]
 
 ebs = pd.concat([covid,non,_54k],ignore_index=True)
 ebs=ebs.drop_duplicates(subset=['TranscriptID'], keep='first').reset_index()
@@ -32,54 +34,55 @@ ebs=ebs.drop_duplicates(subset=['TranscriptID'], keep='first').reset_index()
 ebs=pd.merge(ebs,md,on=["TranscriptID"])
 ebs["ID"]="."
 ebs["score"]="0"
+ebs["source"]="Highly expressed evidence based gene"
+
+ebs["attributes"]=ebs["attributes"]+ebs["COVID-19 expressed"]
+
 
 bedDet=ebs[["chr", "start","end","TranscriptID","score","strand","source","attributes"]]
+
+#For updating metadata
+df=ebs[["TranscriptID","source","COVID-19 expressed"]]
 
 #Make start 0-based
 bedDet["start"]=bedDet["start"].astype(int)-1
 bedDet["end"]=bedDet["end"].astype(int)
 
 
+ 
 
 bed=open("UCSC_EB.bed", "w")
 bed.write('browser position chr1')
-bed.write('\n')   
-bed.write("track name=\"COVID-19 expressed\" type=bedDetail color=255,0,0 description=\"COVID-19 expressed evidence based\" visibility=1")
 bed.write('\n')
-bed.write(bedDet[(bedDet['source'] == 'COVID-19 expressed EB') ].to_csv(index=False, header=False, sep='\t'))
-   
+bed.write("track name=\"Evidence based genes\" type=bedDetail color=0,0,255 description=\"Evidence based\" visibility=1")
 bed.write('\n')
-bed.write("track name=\"Evidence based\" type=bedDetail color=0,0,255 description=\"Evidence based\" visibility=1")
-bed.write('\n')
-bed.write(bedDet[(bedDet['source'] == 'EB') ].to_csv(index=False, header=False, sep='\t'))
+bed.write(bedDet.to_csv(index=False, header=False, sep='\t'))
 bed.close()
 
 
 
 
 
-
-#Update metadata
+#####################################Update metadata
 md_tx = pd.read_csv("Transcript_level_metadata.tsv",sep='\t')  
-md_tx=md_tx.drop(columns=['EB_type'])    
+md_tx=md_tx.drop(columns=['isHighlyExpressed',"COVID-19 expressed"])    
 
 md_gene = pd.read_csv("Gene_level_metadata.tsv",sep='\t') 
-md_gene=md_gene.drop(columns=['EB_type'])  
+md_gene=md_gene.drop(columns=['isHighlyExpressed',"COVID-19 expressed"])
 md_gene=md_gene.drop_duplicates()
-
-df=bedDet[["TranscriptID","source"]]          
-df = df.rename(columns={'source': 'EB_type'}) 
-
+df = df.rename(columns={'source': 'isHighlyExpressed'}) 
+df=df.replace('', np.nan)
 md_tx=pd.merge(md_tx,df,on=["TranscriptID"],how="left") 
-
 md_tx.to_csv("Transcript_level_metadata.tsv",sep='\t',index=False) 
 
-df=md_tx[["EB_type","Gene_ID_ver"]].dropna()
+
+df=md_tx[["isHighlyExpressed","COVID-19 expressed","Gene_ID_ver"]]
+df=df[df["isHighlyExpressed"]=="Highly expressed evidence based gene"]
 df=df.drop_duplicates()
 md_gene=pd.merge(md_gene,df,on=["Gene_ID_ver"],how="left")
 
-md_gene.to_csv("Gene_level_metadata.tsv",sep='\t',index=False)
 
+md_gene.to_csv("Gene_level_metadata.tsv",sep='\t',index=False)
 
 
 
